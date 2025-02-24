ed464498e07db2e874e413bfc6a45af2f124d7aa
Jack <162419234+zhangmcii@users.noreply.github.com>
2025-02-24 06:31:14 +0000

    js和py

---
+++ day.py
@@ -0,0 +1,5 @@
+class Test:
+    def __init__(self,price, discount):
+        self.price = price
+        self.discount = discount
+        
---
+++ skill.js
@@ -0,0 +1,3 @@
+function add(v1,v2){
+    return v1+v2;
+}
